f88acebe47f48ca00a716fef5abd641ddd91a494
Elijah <elijahsharper1@gmail.com>
2018-02-04 22:31:39 +0300

    Broadcasting messages

--- public/chat.js
+++ public/chat.js
@@ -4,7 +4,7 @@ var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
       output = document.getElementById('output');
-
+      feedback = document.getElementById('feedback');
 
 // Emit events
 btn.addEventListener('click', function() {
@@ -14,8 +14,18 @@ btn.addEventListener('click', function() {
   });
 });
 
+message.addEventListener('keypress', function() {
+  socket.emit('typing', {
+    handle: handle.value
+  })
+});
+
 
 // listen for events
 socket.on('chat', function(data) {
   output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data.handle + ' is typing a message...</em></p>';
 });
